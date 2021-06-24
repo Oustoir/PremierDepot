6ace3201ec8d29f148b5e643f4c74faf7579aaaf
Oustoir <houssop4@gmail.com>
2021-06-24 15:46:52 +0200

    premierdepot

---
+++ siteAfpa/js/script.js
@@ -0,0 +1,4 @@
+
+var offset = 100, // browser window scroll (in pixels) after which the "back to top" link is shown
+  offsetOpacity = 100, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+  scrollDuration = 700;
